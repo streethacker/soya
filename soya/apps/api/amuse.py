a41788f2cff4c42cc9045379b55bb3d92d617dd8
streethacker <silverbullet7714@126.com>
2015-04-26 13:50:02 +0800

    [INIT] amuse APIs

--- soya/apps/api/amuse.py
+++ soya/apps/api/amuse.py
@@ -27,7 +27,7 @@ def __reconstruct_movie_info(is_new, is_imax, movie_score, *movies):
     def __apply_filters(movie):
         filters = {
             'is_new': int(is_new) if is_new else None,
-            'is_imax': int(is_imax) if str(is_imax) else None,
+            'is_imax': int(is_imax) if is_imax is not None else None,
             'movie_score': float(movie_score) if movie_score else None,
         }
 
@@ -53,7 +53,7 @@ def __reconstruct_movie_info(is_new, is_imax, movie_score, *movies):
         if not __apply_filters(movie):
             continue
 
-        result.append({
+        movie_info = {
             'name': movie['movie_name'],
             'type': movie['movie_type'],
             'release_date': movie['movie_release_date'],
@@ -64,7 +64,40 @@ def __reconstruct_movie_info(is_new, is_imax, movie_score, *movies):
             'director': movie['movie_director'],
             'tags': movie['movie_tags'],
             'message': movie['movie_message'],
-            'picture': movie['movie_big_picture'],
+        }
+
+        picture = movie.get('movie_big_picture') or movie.get('movie_picture')
+        time_table = movie.get('time_table')
+
+        movie_info.update({
+            'picture': picture,
+            'time_table': time_table,
+        })
+
+        result.append(movie_info)
+
+    return result
+
+
+def __reconstruct_cinema_info(movie_score, *cinemas):
+
+    result = []
+
+    for cinema in cinemas:
+        movies = __reconstruct_movie_info(
+            None,
+            None,
+            movie_score,
+            *cinema['movies']
+        )
+
+        result.append({
+            'name': cinema['name'],
+            'telephone': cinema['telephone'],
+            'location': cinema['location'],
+            'address': cinema['address'],
+            'rating': cinema['rating'],
+            'movies': movies,
         })
 
     return result
@@ -85,3 +118,24 @@ def get_hot_movie(location, is_new=None, is_imax=None, movie_score=None):
         jsonify()['result']['movie']
 
     return __reconstruct_movie_info(is_new, is_imax, movie_score, *results)
+
+
+@bp.route('/movie/search/cinema')
+@render_json
+@validator({
+    'location': StringField,
+    'cinema_name': StringField,
+    'movie_score': FloatField,
+})
+def search_cinema(location, cinema_name, movie_score=None):
+
+    kwargs = {
+        'location': location,
+        'wd': cinema_name,
+        'qt': 'search_cinema',
+    }
+
+    results = SoyaToolkit().query('movie', **kwargs).\
+        jsonify()['result']
+
+    return __reconstruct_cinema_info(movie_score, *results)
